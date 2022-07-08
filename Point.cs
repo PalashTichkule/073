8f9f8bb65889825d1e1cdb0bba3dfdc866f36df6
PranjalTichkule <107771081+PranjalTichkule@users.noreply.github.com>
2022-07-08 16:15:45 +0530

    Create Point.cs

---
+++ Point.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Library
+{
+    public struct Point
+    {
+        public int x;
+        public int y;
+
+        public Point(int x,int y)
+        {
+          this.x = x;
+          this.y = y;
+        }
+       public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public override string ToString()
+        {
+            string result = string.Format("X={0} Y={1}",this.x,this.y);
+            return result;
+        }
+
+    }
+}
